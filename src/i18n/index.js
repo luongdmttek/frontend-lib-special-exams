dd508eef5917bdc0f9900e9d20795285eb2c7c39
luongdmttek <43735946+luongdmttek@users.noreply.github.com>
2023-03-30 21:35:16 +0700

    update

---
+++ src/i18n/index.js
@@ -0,0 +1,8 @@
+import viMessages from './messages/vi.json';
+// no need to import en messages-- they are in the defaultMessage field
+
+const messages = {
+  vi: viMessages,  
+};
+
+export default messages;
