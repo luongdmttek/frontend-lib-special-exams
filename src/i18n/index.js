a3d12725e62c4f42dbe7c62267a358d9787fdb2b
luongdmttek <luong.ngo@dmttek.com>
2023-03-31 15:19:52 +0700

    update

--- src/i18n/index.js
+++
@@ -1,8 +0,0 @@
-import viMessages from './messages/vi.json';
-// no need to import en messages-- they are in the defaultMessage field
-
-const messages = {
-  vi: viMessages,  
-};
-
-export default messages;
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,2 @@
-import messages from './i18n/index';
 export { default } from './core/SequenceExamWrapper';
 export { default as OuterExamTimer } from './core/OuterExamTimer';
-export { messages };
